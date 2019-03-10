4914af257689b400cb807dc2675ae9367eff4e81
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2019-03-10 23:25:59 +0200

    Better grep

--- system.sh
+++ system.sh
@@ -33,7 +33,7 @@ function unpkg {
 function create-groups {
     local ALL_GROUPS=$(cut -d: -f1 /etc/group)
     for group in "$@"; do
-        if ! echo "$ALL_GROUPS" | grep -q ^${group}$; then
+        if ! echo "$ALL_GROUPS" | grep -qw ${group}; then
             sudo groupadd ${group}
         fi
     done
@@ -41,7 +41,7 @@ function create-groups {
 
 function add-user-to-groups {
     for group in "$@"; do
-        if ! groups ${USER} | grep -q $group; then
+        if ! groups ${USER} | grep -qw $group; then
             sudo gpasswd --add ${USER} ${group}
         fi
     done
