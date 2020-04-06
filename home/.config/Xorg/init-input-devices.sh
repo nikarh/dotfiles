151bd6501db32aad653acc4cd74b9db319954d6b
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2020-04-06 10:24:23 +0300

    Logitech mouse accel

--- home/.config/Xorg/init-input-devices.sh
+++ home/.config/Xorg/init-input-devices.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 xinput --set-prop 'pointer:MSI Gaming Mouse' 'libinput Accel Speed' 0.9
+xinput --set-prop 'ImPS/2 Logitech Wheel Mouse' 'libinput Accel Speed' 0.5
 
 setxkbmap $(cat ~/.config/Xorg/Xkbmap)
 xset r rate 200 60
