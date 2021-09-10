5c049063ce12e6e0dd8b882fb4a7adaf6c4c4364
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2021-09-10 20:11:11 +0300

    More wayland

--- profiles/workstation/user/home/.bin/toggle-touchpad.sh
+++ profiles/workstation/user/home/.bin/toggle-touchpad.sh
@@ -3,17 +3,32 @@
 cd "$(dirname "$(readlink -f "$0")")" || exit
 source ./functions.sh
 
-DEVICE_ID=$(xinput list | grep -Eoi 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
-DEVICE_STATE=$(xinput list-props "$DEVICE_ID" | grep 'Device Enabled' | awk '{print $4}')
+ENABLED=0
+if loginctl show-session 2 -p Type | grep -q wayland 2> /dev/null; then
+    WAYLAND=true
+    ENABLED=$(swaymsg -t get_inputs | jq -r -c '[.[] | select(.type == "touchpad").libinput.send_events][0]' | grep -cw enabled)
+else 
+    DEVICE_ID=$(xinput list | grep -Eoi 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
+    ENABLED=$(xinput list-props "$DEVICE_ID" | grep 'Device Enabled' | awk '{print $4}' | grep -cw 1)
+fi
 
-if [[ "$DEVICE_STATE" -eq 1 ]]; then
-    xinput disable "$DEVICE_ID"
+if [[ "$ENABLED" == "1" ]]; then
+    if $WAYLAND; then
+        swaymsg input type:touchpad events disabled
+    else
+        xinput disable "$DEVICE_ID"
+    fi
+    swaymsg input type:touchpad events disabled
 
     ICON=touchpad-disabled-symbolic \
     SUMMARY="Touchpad disabled" \
         notify
 else
-    xinput enable "$DEVICE_ID"
+    if $WAYLAND; then
+        swaymsg input type:touchpad events enabled
+    else
+        xinput enable "$DEVICE_ID"
+    fi
 
     ICON=touchpad-enabled-symbolic \
     SUMMARY="Touchpad enabled" \
