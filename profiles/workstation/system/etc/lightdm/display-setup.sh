10362989fa0410699abb0b40c16228cda5903a45
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2021-09-16 11:42:57 +0300

    Fixed display-setup

--- profiles/workstation/system/etc/lightdm/display-setup.sh
+++ profiles/workstation/system/etc/lightdm/display-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if xrandr --listproviders | grep -s "NVIDIA-0"; then
+if loginctl show-session 2 -p Type | grep -qv wayland 2> /dev/null; then
+  if xrandr --listproviders | grep -s "NVIDIA-0"; then
     xrandr --setprovideroutputsource modesetting NVIDIA-0
+  fi
+
+  xrandr --auto
 fi
-
-xrandr --auto
-
