38f569721e0c6a3f6c226a76e89c67be6f068560
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2022-05-12 16:13:12 +0300

    Jellyfin auth

--- profiles/server/system/var/lib/hass/auth.sh
+++ profiles/server/system/var/lib/hass/auth.sh
@@ -2,6 +2,8 @@
 set -e
 
 authelia=http://authelia:8080
+hass=https://hass.home.arhipov.net
+
 username="$(echo -n "$username" | jq -aRs .)"
 password="$(echo -n "$password" | jq -aRs .)"
 
@@ -11,7 +13,7 @@ response="$(curl -si -w "\n%{size_header},%{size_download}" \
     -d '{
        "username": '"$username"',
        "password": '"$password"',
-       "targetURL": "https://hass.home.arhipov.net",
+       "targetURL": "'"$hass"'",
        "requestMethod": "GET",
        "keepMeLoggedIn": true
     }')"
@@ -22,14 +24,22 @@ headers="${response:0:${headerSize}}"
 body="${response:${headerSize}:${bodySize}}"
 cookie="$(grep -i ^set-cookie <<< "$headers" | cut -c 13-)"
 
-if [[ "$(echo $body | jq -r .status)" == "OK" ]]; then
-    response="$(curl -s \
-        -b "$cookie" \
-        "$authelia/api/user/info"
-    )"
-    display_name="$(echo "$response" | jq -r .data.display_name)"
-
-    echo name = $display_name
-    exit 0
+if [[ "$(echo $body | jq -r .status)" != "OK" ]]; then
+    exit 1
 fi
-exit 1
+
+# Verify that user has access to hass
+curl -sf \
+    -b "$cookie" \
+    -H 'X-Original-Url: '$hass'' -H 'X-Forwarded-Method: GET' \
+    "$authelia/api/verify"
+
+# Get display name
+response="$(curl -sf \
+    -b "$cookie" \
+    "$authelia/api/user/info"
+)"
+display_name="$(echo "$response" | jq -r .data.display_name)"
+
+echo name = $display_name
+exit 0
