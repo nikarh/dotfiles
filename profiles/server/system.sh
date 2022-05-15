700c646db54075e8f1d50511260bdaaa268c0da5
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2022-05-15 19:54:03 +0300

    Samba in docker with authelia PAM auth

--- profiles/server/system.sh
+++ profiles/server/system.sh
@@ -4,10 +4,8 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 sudo timedatectl set-timezone Europe/Riga
 sudo timedatectl set-ntp true
 
-pkg mdadm hdparm \
-    haveged \
-    docker fuse-overlayfs docker-compose \
-    dhclient samba \
+pkg mdadm hdparm haveged \
+    docker fuse-overlayfs docker-compose dhclient \
     certbot certbot-systemd
 
 sudo systemctl mask mdmonitor
@@ -27,28 +25,22 @@ sudo chmod 600 /var/lib/sftpd/secrets/ssh*
 # Append bind mounts
 sudo mkdir -p /var/data/{shares,home}
 mkdir -p /var/data/shares/tmp/ssd
-sudo mkdir -p /var/data/home/{nikarh,anastasiia}/{data,shared}
-sudo chown files:files /var/data/{shares,home}/*
-sudo chmod 700 /var/data/home/{nikarh,anastasiia}
+sudo mkdir -p /var/data/home/{shield/shared,{nikarh,anastasiia}/{data,shared}}
+
+# Fix permissions for PAM authentication
+sudo chown files:files /var/data/shares/*
+sudo chown files:files /var/data/home/*/*
+sudo chmod 755 /var/data/home/*
 
 sudo sed -i '/# BEGIN mounts/,/# END mounts/d' /etc/fstab
 cat "$ROOT/system/fstab" | sudo tee -a /etc/fstab > /dev/null
 sudo systemctl daemon-reload
 sudo mount -a
 
-# Create users for smb
-sudo groupadd -r -g 65533 shield 2> /dev/null || true;
-sudo useradd -M -u 65533 -g 65533 -s /usr/bin/nologin shield 2> /dev/null || true;
-sudo useradd -MU -s /usr/bin/nologin {nikarh,anastasiia} 2> /dev/null || true;
-
-# Add smb users
-while read -r line || [ -n "$line" ]; do
-    IFS=':' read -r username password <<< "$line"
-    echo -e "$password\n$password\n" | sudo pdbedit -a "$username" -t > /dev/null
-done < "$ROOT/system/smbusers"
-
+# Start services
 sudo systemctl enable --now docker
 enable-units \
-    dhclient@eno1 systemd-timesyncd sshd smb nmb
+    dhclient@eno1 systemd-timesyncd sshd
 
+sudo docker-compose --project-directory="$ROOT" build
 sudo docker-compose --project-directory="$ROOT" up -d
--- profiles/server/system/var/lib/sftpd/run.sh
+++ profiles/server/system/var/lib/sftpd/run.sh
@@ -7,7 +7,7 @@ fi
 rm -f /etc/sftp/users.conf
 mkdir -p /etc/sftp
 
-for f in /home/*; do 
+for f in /home/*; do
   echo "$(basename $f)::1000:1000" >> /etc/sftp/users.conf;
 done
 
