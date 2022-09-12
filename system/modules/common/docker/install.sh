80dea53630c846f174f59117b8add42d48d9fab4
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2022-09-12 19:48:54 +0300

    Closer to working homekit

--- system/modules/common/docker/install.sh
+++ system/modules/common/docker/install.sh
@@ -2,10 +2,9 @@
 
 pkg docker docker-compose
 
-if pacman -Q nvidia > /dev/null || pacman -Q nvidia-open > /dev/null; then
+if pacman -Q nvidia > /dev/null 2>&1 || pacman -Q nvidia-open > /dev/null 2>&1; then
     pkg nvidia-container-toolkit
 fi
-
 
 sudo systemctl enable docker.service
 
