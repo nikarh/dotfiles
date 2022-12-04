4743640400ad7b533d36fd13e44a0dc83955fd38
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2022-12-04 02:26:57 +0200

    Backups

--- system/modules/server/opiz/install.sh
+++ system/modules/server/opiz/install.sh
@@ -3,12 +3,13 @@
 # Copy all system configs
 sudo cp -ufrTv "$ROOT/root/" /
 
-sudo apt update
-sudo apt install docker.io docker-compose
-sudo apt upgrade
+# sudo apt update
+# sudo apt install docker.io docker-compose
+# sudo apt upgrade
 
 docker-compose --project-directory="$ROOT" \
     --env-file "$ROOT/.env" \
     -f "$ROOT/docker-compose.yaml" \
+    -f "$ROOT/backup.docker-compose.yaml" \
     -f "$ROOT/../common/docker-compose.yaml" \
     up -d
