18a3d8f0146f4957974c5fbfa6374eaf9c887f23
Nikolay Arhipov <n@arhipov.net>
2023-03-21 19:31:25 +0200

    Migrated sftpd to ubuntu from alpine because it broke

--- system/modules/server/files/root/var/lib/docker-services/volumes/sftpd/run.sh
+++ system/modules/server/files/root/var/lib/docker-services/volumes/sftpd/run.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 if ! which jq >/dev/null; then
-  apk add --no-cache jq curl
+  if which jq >/dev/null; then
+    apk add --no-cache jq curl
+  else
+    apt-get update -y
+    apt-get install -y curl jq
+    rm -rf /var/lib/apt/lists
+  fi
 fi
 
 rm -f /etc/sftp/users.conf
 mkdir -p /etc/sftp
 
 for f in /home/*; do
-  echo "$(basename $f)::1000:1000" >> /etc/sftp/users.conf;
+  echo "$(basename $f):stub:1000:1000" >> /etc/sftp/users.conf;
 done
 
 /entrypoint
