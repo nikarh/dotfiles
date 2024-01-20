bc322826017f2b5f5f26e37f6628bcda7ff275a8
Nikolay Arhipov <n@arhipov.net>
2024-01-20 14:32:36 +0200

    lsd -> exa

--- system/modules/common/console-tools/install.sh
+++ system/modules/common/console-tools/install.sh
@@ -4,7 +4,7 @@ pkg zip unzip p7zip unrar \
     man-db man-pages \
     neovim tmux \
     bash-completion starship git-delta grc fzf \
-    lsd zoxide lazygit fd ripgrep bat \
+    eza zoxide lazygit fd ripgrep bat \
     socat bandwhich traceroute \
     parallel lsof usbutils rsync htop iotop \
     wget jq go-yq croc rclone \
