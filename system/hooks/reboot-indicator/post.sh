39b21d95cc0ab416610ecf7e26bf5306f5346e94
Nikolay Arhipov <n@arhipov.net>
2024-01-21 18:21:24 +0200

    Better reboot indicator

--- system/hooks/reboot-indicator/post.sh
+++ system/hooks/reboot-indicator/post.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-export NEW_REBOOT_HASH="$(pacman -Q linux linux-lts nvidia nvidia-utils 2>/dev/null | sha1sum)"
-
-if [[ "$REBOOT_HASH" != "$NEW_REBOOT_HASH" ]]; then
+if [[ "$PRE_REBOOT_HASH" != "$(reboot-hash)" ]]; then
     touch /tmp/reboot-indicator
 fi
--- system/hooks/reboot-indicator/pre.sh
+++ system/hooks/reboot-indicator/pre.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-export REBOOT_HASH="$(pacman -Q linux linux-lts nvidia nvidia-lts nvidia-dpms nvidia-utils 2>/dev/null | sha1sum)"
+function reboot-hash {
+    pacman -Q linux linux-lts nvidia nvidia-lts nvidia-dpms nvidia-utils 2>/dev/null | sha1sum
+}
+
+export PRE_REBOOT_HASH="$(reboot-hash)"
