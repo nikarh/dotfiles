c4bdedc30620c25ca46c9cfb525084b1092b2e08
Nikolay Arhipov <n@arhipov.net>
2026-02-12 13:44:12 +0200

    Reworked docker setup
    - Put relevant container groups to individual subnets
    - Made most containers rootless
    - Made most containers read_only
    - All containers use "no-new-privileges:true"
    - Most containers drop ALL caps
    - Upgraded some images

---
+++ system/modules/server/files/root/var/lib/docker-services/volumes/feishin/settings.js
@@ -0,0 +1,7 @@
+"use strict";
+window.SERVER_URL = "https://media.files.home.arhipov.net";
+window.SERVER_NAME = "jellyfin";
+window.SERVER_TYPE = "jellyfin";
+window.SERVER_LOCK = "true";
+window.LEGACY_AUTHENTICATION = "false";
+window.ANALYTICS_DISABLED = "true";
